cbb88b86b6d0f41aa52538d901aabb77cb3c4aa4
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-09 23:01:04 +0530

    footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
             <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
             <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
             <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-            <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+            <a href="https://github.com/MuniPanugothu01?tab=repositories" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
           </div>
         </div>
       </div>
